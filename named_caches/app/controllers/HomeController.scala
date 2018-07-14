65652301850401387e65bdc17ed9eed04763c662
twr143 <twr143@gmail.com>
2018-07-14 19:35:37 +0300

    Initial commit, 1

---
+++ named_caches/app/controllers/HomeController.scala
@@ -0,0 +1,63 @@
+package controllers
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.inject._
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import play.api.cache._
+import play.api.cache.redis.CacheAsyncApi
+import play.api.mvc._
+import scala.concurrent.duration._
+
+@Singleton
+class HomeController @Inject()
+(
+  // default unqualified instance, equal to "local"
+  cache: CacheAsyncApi,
+  // instance with DB 1
+  @NamedCache("local") local: CacheAsyncApi,
+  // instance with DB 2
+  @NamedCache("remote") remote: CacheAsyncApi,
+  // instance with a custom recovery policy
+  @NamedCache("failing") failing: CacheAsyncApi,
+  cc: ControllerComponents
+)(implicit executionContext: ExecutionContext) extends AbstractController(cc) {
+  import Imports._
+  private def message(instance: CacheAsyncApi, name: String) = instance.getOrElse("named-caches#message", expiration = 10.seconds) {
+    s"This message was set to $name instance at ${now.asString}."
+  }
+
+  private def action(instance: CacheAsyncApi, name: String) = instance.get[String]("named-caches#message").map {
+    case Some(value) => s"The value is $value"
+    case None =>
+      instance.set("named-caches#message", 1, 5.seconds)
+      s"no cache available for the message $name"
+  }
+
+  private def remove(instance: CacheAsyncApi, name: String) = instance.remove("named-caches#message")
+
+  private def messageInDefault = action(cache, "default")
+
+  private def messageInLocal = action(local, "local")
+
+  private def messageInRemote = action(remote, "remote")
+
+  def messageInFailing = action(failing, "failing")
+
+  def index = Action.async {
+    for {
+      default <- this.messageInDefault
+      local <- this.messageInLocal
+      remote <- this.messageInRemote
+      failing <- this.messageInFailing
+    } yield {
+      Ok(views.html.index(default, local, remote, failing, now.asString))
+    }
+  }
+}
+object Imports {
+  implicit class RichDate(val date: LocalDateTime) extends AnyVal {
+    def asString = DateTimeFormatter.ofPattern("HH:mm:ss").format(date)
+  }
+  def now = LocalDateTime.now()
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,5 @@
+// checks for updates
+//addSbtPlugin( "com.timushev.sbt" % "sbt-updates" % "0.3.4" )
+// play framework
+addSbtPlugin( "com.typesafe.play" % "sbt-plugin" % "2.6.15")
+
